ee1395f060dbe10b098a66adc3951fb63dfe49e3
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-18 22:02:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string){
     return "YES INDEED"
   }
   if(string.toLowerCase() === string){
-    return "I can\'t hear you."
+    return "I can\'t hear you!"
   }
   return "I can\'t hear you!"
 }
