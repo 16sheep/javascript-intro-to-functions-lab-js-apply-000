bdaaba85353ff9029505af9722d71c4af871db9d
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-18 22:02:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,8 +17,14 @@ function logWhisper(string) {
 
 
 function sayHiToGrandma(string){
-  if(string.toUpperCase() === string){
-    return "I love you, Grandma."
+    if(string === "I love you Grandma."){
+    return "I love you too!"
+  }
+    if(string.toUpperCase() === string){
+    return "YES INDEED"
+  }
+  if(string.toLowerCase() === string){
+    return "I can\'t hear you."
   }
   return "I can\'t hear you!"
 }
